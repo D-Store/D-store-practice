efc9bb88d0a12e915fae0ae34d0f15f73e92d62a
toswm128 <chominsu10@gmail.com>
2021-04-08 20:36:23 +0900

    [ADD] router

--- src/Screen/App.js
+++ src/Screen/App.js
@@ -1,9 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      react
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component="" />
+      </Switch>
+    </Router>
   );
 }
 
