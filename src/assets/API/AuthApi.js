9dd9b31234dfdfd30aae9a2375114e33d3d55720
toswm128 <chominsu10@gmail.com>
2021-04-09 23:20:07 +0900

    [ADD] signUpApi

---
+++ src/assets/API/AuthApi.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { SERVER } from "../../config/config.json";
+class AuthApi {
+  constructor() {}
+
+  async login(userId, password) {
+    const body = {
+      userId,
+      password,
+    };
+    const response = await axios.post(`${SERVER}/signin`, body);
+
+    return response;
+  }
+
+  async signUp(userId, password, name, genderId) {
+    const body = {
+      userId,
+      password,
+      name,
+      genderId,
+    };
+    const response = await axios.post(`${SERVER}/signup`, body);
+
+    return response;
+  }
+}
+
+export default new AuthApi();
