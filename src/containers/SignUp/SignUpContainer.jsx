0b7516e4e74c2334d0dcf3e87c1a80e045dd2e0e
toswm128 <chominsu10@gmail.com>
2021-04-09 10:24:52 +0900

    [ADD] signUpEnterEvent

--- src/containers/SignUp/SignUpContainer.jsx
+++ src/containers/SignUp/SignUpContainer.jsx
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import SignUp from "../../components/SignUp";
 
 const SignUpContainer = () => {
-  return <SignUp />;
+  const passwordInput = useRef();
+  const nameInput = useRef();
+  const [Userid, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const onChangeUserId = e => {
+    setUserId(e.target.value);
+  };
+  const onChangeUserPw = e => {
+    setPassword(e.target.value);
+  };
+  const onChangeUserName = e => {
+    setName(e.target.value);
+  };
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Enter" || e.key === "NumpadEnter") {
+        if (name.length !== 0) {
+          console.log("회원가입!");
+        } else if (password.length !== 0) {
+          nameInput.current?.focus();
+        } else {
+          passwordInput.current?.focus();
+        }
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, [Userid, password, name]);
+  return (
+    <SignUp
+      Userid={Userid}
+      password={password}
+      name={name}
+      onChangeUserId={onChangeUserId}
+      onChangeUserPw={onChangeUserPw}
+      onChangeUserName={onChangeUserName}
+      passwordInput={passwordInput}
+      nameInput={nameInput}
+    />
+  );
 };
 
 export default SignUpContainer;
