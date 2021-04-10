3b72d7f8d1de20947158a5fea45641ed2e77862a
toswm128 <chominsu10@gmail.com>
2021-04-10 23:50:50 +0900

    [ADD] PostApi

---
+++ src/assets/API/PostApi.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { SERVER } from "../../config/config.json";
+class PostApi {
+  async getBoard(boardIds) {
+    const response = await axios.get(`${SERVER}/board?boardIds=[${boardIds}]`, {
+      headers: {
+        Authorization: localStorage.getItem("accessToken"),
+      },
+    });
+
+    return response;
+  }
+  async CreateBoard(date, contents, profile, show) {
+    const body = {
+      date,
+      contents,
+      profile,
+      show,
+    };
+    const response = await axios.post(`${SERVER}/board`, body, {
+      headers: {
+        Authorization: localStorage.getItem("accessToken"),
+        "content-type": "multipart/form-data",
+      },
+    });
+
+    return response;
+  }
+}
+
+export default new PostApi();
