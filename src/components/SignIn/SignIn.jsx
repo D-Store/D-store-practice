7ed1301acf118ce848418fa0964994cb290306e2
leeminewook <63051680+leeminewook@users.noreply.github.com>
2021-04-12 14:16:42 +0900

    [UPDATE]SignIn

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
+import './SignIn.css'
 
 const SignIn = ({
     userId,
     password,
     onChangeUserId,
     onChangeUserPw,
-    tryLogin
+    tryLogin,
+    passwordInput,
+    errorInput
 }) => {
     return(
         <>
-        <div>
-            <input 
-            type="text" 
-            name="userId" 
-            placeholder="아이디" 
-            onChange={(e) => onChangeUserId(e)} 
-            value={userId}></input>
+        <div className="MainSignInDiv">
+            <div>
+                <input 
+                type="text" 
+                name="userId" 
+                placeholder="아이디" 
+                onChange={(e) => onChangeUserId(e)} 
+                value={userId}></input>
+            </div>
+            <div 
+            className="errorInput" 
+            style={{display:"none"}}
+            ref={errorInput}
+            >
+                아이디를 입력해주세요
+            </div>
+            <div>
+                <input 
+                type="password" 
+                name="userPw" 
+                placeholder="비밀번호"
+                ref={passwordInput} 
+                onChange={(e) => onChangeUserPw(e)} 
+                value={password}/>
+            </div>
+            <div onClick={tryLogin}>로그인</div>
         </div>
-        <div>
-            <input type="password" name="userPw" placeholder="비밀번호" onChange={(e) => onChangeUserPw(e)} value={password}/>
-        </div>
-        <div onClick={tryLogin}>로그인</div>
         </>
     )
 }
--- src/containers/SignIn/SingInContainer.jsx
+++ src/containers/SignIn/SingInContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SignIn from "../../components/SignIn/SignIn";
 import { useHistory } from "react-router-dom";
 import AuthApi from "../../assets/API/AuthApi";
@@ -6,8 +6,10 @@ import AuthApi from "../../assets/API/AuthApi";
 const SingInContainer = () => {
   const history = useHistory();
 
-  const [userId, setUserId] = useState("");
+  const passwordInput = useRef();
+  const errorInput = useRef();
 
+  const [userId, setUserId] = useState("");
   const [password, setUserPw] = useState("");
 
   const onChangeUserId = e => {
@@ -18,19 +20,35 @@ const SingInContainer = () => {
     setUserPw(e.target.value);
   };
 
+  useEffect(()=>{
+    const listener = (e) => {
+      if(e.key === "Enter"|| e.key === "NumpadEnter"){
+        if(userId.length === 0){
+          console.log(errorInput.current.style.display)
+          errorInput.current.style.display = "block"
+        }
+      }
+    }
+    document.addEventListener("keydown",listener);
+    return () => {
+      document.removeEventListener("keydown",listener);
+    }
+  })
+
   const tryLogin = async () => {
     try {
       if (!userId || !password) {
         alert("아이디 또는 비밀번호를 입력하지 않았습니다.");
       } else {
         const response = await AuthApi.login(userId, password);
-        //response.config.data
         console.log(response.config.data.userId);
         if (response.status === 200) {
           alert("로그인을 성공하였습니다.");
           localStorage.setItem("accessToken", response.data.token);
           if (userId === "test" && password === "1234") {
             history.push("/admin/main");
+          }else{
+            history.push("/");
           }
         } else {
           alert("로그인을 실패하였습니다.");
@@ -48,6 +66,8 @@ const SingInContainer = () => {
       onChangeUserId={onChangeUserId}
       onChangeUserPw={onChangeUserPw}
       tryLogin={tryLogin}
+      passwordInput={passwordInput}
+      errorInput={errorInput}
     />
   );
 };
