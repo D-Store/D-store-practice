1cc2559577b19a2b06b5654f29ef352e5560fa5b
leeminewook <63051680+leeminewook@users.noreply.github.com>
2021-04-13 10:03:16 +0900

    [ADD]SignIn Css

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './SignIn.css'
 
 const SignIn = ({
@@ -15,50 +16,67 @@ const SignIn = ({
 }) => {
     return(
         <>
-        <div className="MainSignInDiv">
-            <div>
-                <input 
-                type="text" 
-                name="userId" 
-                placeholder="아이디" 
-                ref={userIdInput}
-                onChange={(e) => onChangeUserId(e)} 
-                value={userId}></input>
+        <div className="signIn">
+            <div className="signIn-form">
+                <div className="signIn-form-title">로그인</div>
+                <div className="signIn-input">
+                    <div>
+                        <input
+                        className="signIn-input-item"
+                        type="text" 
+                        name="userId" 
+                        placeholder="아이디" 
+                        ref={userIdInput}
+                        onChange={(e) => onChangeUserId(e)} 
+                        value={userId}></input>
+                    </div>
+                    <div 
+                    className="errorMessage"
+                    name="errorDiv"
+                    style={{display:"none"}}
+                    ref={errorInputUserId}
+                    >
+                        아이디를 입력해주세요
+                    </div>
+                    <div>
+                        <input 
+                        className="signIn-input-item"
+                        type="password" 
+                        name="userPw" 
+                        placeholder="비밀번호"
+                        ref={passwordInput} 
+                        onChange={(e) => onChangePassword(e)} 
+                        value={password}/>
+                    </div>
+                    <div 
+                    className="errorMessage" 
+                    name="errorDiv"
+                    style={{display:"none"}} 
+                    ref={errorInputPassword}
+                    >
+                        비밀번호를 입력해주세요
+                    </div>
+                    <div 
+                    className="errorMessage"
+                    name="errorDiv"
+                    style={{display:"none"}}
+                    ref={errorTryLogin}
+                    >
+                        가입하지 않은 아이디이거나,잘못된 비밀번호입니다.
+                    </div>
+                    <div 
+                    className="signIn-submit" 
+                    onClick={tryLogin}
+                    >
+                        로그인
+                    </div>
+                    <div className="another-approach">
+                        <span className="another-approach-item">
+                            아직 계정이 없으신가요? <Link className="another-approach-item-Link" to="/signUp">회원가입</Link>
+                        </span>
+                    </div>
+                </div>
             </div>
-            <div 
-            className="errorMessage"
-            name="errorDiv"
-            style={{display:"none"}}
-            ref={errorInputUserId}
-            >
-                아이디를 입력해주세요
-            </div>
-            <div>
-                <input 
-                type="password" 
-                name="userPw" 
-                placeholder="비밀번호"
-                ref={passwordInput} 
-                onChange={(e) => onChangePassword(e)} 
-                value={password}/>
-            </div>
-            <div 
-            className="errorMessage" 
-            name="errorDiv"
-            style={{display:"none"}} 
-            ref={errorInputPassword}
-            >
-                비밀번호를 입력해주세요
-            </div>
-            <div 
-            className="errorMessage"
-            name="errorDiv"
-            style={{display:"none"}}
-            ref={errorTryLogin}
-            >
-                가입하지 않은 아이디이거나,잘못된 비밀번호입니다.
-            </div>
-            <div onClick={tryLogin}>로그인</div>
         </div>
         </>
     )
