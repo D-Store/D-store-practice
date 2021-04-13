8cf8d423ccc89ed28824cef820bb36ab47979dec
leeminewook <63051680+leeminewook@users.noreply.github.com>
2021-04-13 21:54:34 +0900

    [ADD]Admin css

--- src/components/Admin/AdminMain.jsx
+++ src/components/Admin/AdminMain.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './AdminMain.css'
 
 const AdminMain = () => {
     return(
-        <div>
-            asdf
+        <div className="admin-main-form">
+            <div className="admin-main-title">
+                    유저 정보
+            </div>
+            <div className="admin-main-text">
+                회원정보표시
+            </div>
         </div>
     )
 }
--- src/containers/Admin/AdminMainContainer.jsx
+++ src/containers/Admin/AdminMainContainer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import AdminMain from '../../components/Admin/AdminMain';
+import Header from '../../components/common/Header/Header'
 
 const AdminMainContainer = () => {
     return(
+        <>
+        <Header />
         <AdminMain />
+        </>
     )
 }
 
