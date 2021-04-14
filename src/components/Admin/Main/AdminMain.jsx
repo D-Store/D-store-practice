673a9b6ce93e715d940bafdbbdf8ccf3c4154672
leeminewook <63051680+leeminewook@users.noreply.github.com>
2021-04-14 14:38:24 +0900

    [UPDATE]adminMain form

--- src/components/Admin/Main/AdminMain.jsx
+++ src/components/Admin/Main/AdminMain.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import './AdminMain.css'
 
-const AdminMain = () => {
+const AdminMain = ({
+    changeClassUser,
+    changeClassAdmin,
+    onClickChangeUserCss,
+    onClickChangeAdminCss
+}) => {
     return(
         <div className="admin-main-form">
             <div className="admin-main-title">
                     유저 정보
             </div>
             <div className="admin-main-text">
-                회원정보표시
+                <div className="admin-main-text-title">
+                    <div 
+                    className={changeClassUser ? "admin-main-text-title-item-user clicked":"admin-main-text-title-item-user"}
+                    onClick={()=>{onClickChangeUserCss()}}
+                    >
+                        유저
+                    </div>
+                    <div 
+                    className={changeClassAdmin ? "admin-main-text-title-item-admin clicked":"admin-main-text-title-item-admin"}
+                    onClick={()=>{onClickChangeAdminCss()}}
+                    >
+                        어드민
+                    </div>
+                </div>
+                <div className="admin-main-text-items">
+                    유저
+                </div>
+                <div className="admin-main-text-items">
+                    유저
+                </div>
                 <div className="admin-main-text-items">
                     유저
                 </div>
@@ -18,6 +42,28 @@ const AdminMain = () => {
                 <div className="admin-main-text-items">
                     유저
                 </div>
+                <div className="admin-main-text-items">
+                    유저
+                </div>
+                <div className="admin-main-text-items">
+                    유저
+                </div>
+                <div className="admin-main-text-items">
+                    유저
+                </div>
+                <div className="admin-main-text-items">
+                    유저
+                </div>
+                <div className="admin-main-text-items">
+                    유저
+                </div>
+            </div>
+            <div className="admin-main-page-div">
+                <div className="admin-main-page-div-items">1</div>
+                <div className="admin-main-page-div-items">2</div>
+                <div className="admin-main-page-div-items">3</div>
+                <div className="admin-main-page-div-items">4</div>
+                <div className="admin-main-page-div-items">5</div>
             </div>
         </div>
     )
--- src/containers/Admin/Main/AdminMainContainer.jsx
+++ src/containers/Admin/Main/AdminMainContainer.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdminMain from '../../../components/Admin/Main/AdminMain';
 import Header from '../../../components/common/Header/Header'
 
 const AdminMainContainer = () => {
+
+    const [changeClassUser,setChangeClassUser] = useState(true);
+    const [changeClassAdmin,setChangeClassAdmin] = useState(false);
+
+    const onClickChangeUserCss = (e) => {
+        setChangeClassUser(true)
+        setChangeClassAdmin(false)
+    }
+
+    const onClickChangeAdminCss = () => {
+        setChangeClassUser(false)
+        setChangeClassAdmin(true)
+    }
+
     return(
         <>
         <Header />
-        <AdminMain />
+        <AdminMain 
+        changeClassUser={changeClassUser}
+        changeClassAdmin={changeClassAdmin}
+        onClickChangeUserCss={onClickChangeUserCss}
+        onClickChangeAdminCss={onClickChangeAdminCss}
+        />
         </>
     )
 }
