ee6ab5efde0a0b3ff313b067def1589346251977
조민수 <chominsu10@gmail.com>
2021-04-19 22:27:20 +0900

    [ADD] MainStyle

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Main.css";
 
 const Main = ({ bannerPage, postPage }) => {
   return (
     <div className="main">
-      <div className="main-banner" ref={bannerPage} />
+      <div className="main-banner" ref={bannerPage}>
+        <div className="main-banner-item" />
+      </div>
       <div className="main-posting" ref={postPage}>
         <div className="main-posting-item">
-          <div className="main-posting-item-img"></div>
-          <div className="main-posting-item-img"></div>
-          <div className="main-posting-item-img"></div>
-          <div className="main-posting-item-img"></div>
+          <div className="main-posting-title">당신이 원하는 프로젝트</div>
+          <Link>
+            <div className="main-posting-item-img"></div>
+          </Link>
+          <Link>
+            <div className="main-posting-item-img"></div>
+          </Link>
+          <Link>
+            <div className="main-posting-item-img"></div>
+          </Link>
+          <Link>
+            <div className="main-posting-item-img"></div>
+          </Link>
         </div>
         <div className="main-posting-nav">
           <button className="main-posting-nav-item">«</button>
--- src/containers/Main/MainContainer.jsx
+++ src/containers/Main/MainContainer.jsx
@@ -6,7 +6,6 @@ const MainContainer = () => {
   const bannerPage = useRef();
   const postPage = useRef();
 
-  const [scrollPoint, setScrollPoint] = useState(0);
   const [boradId, setBoradId] = useState([-1]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -44,11 +43,7 @@ const MainContainer = () => {
       window.removeEventListener("wheel", handleScroll, true);
       // 해당 컴포넌트가 언마운트 될때, 스크롤 이벤트를 제거합니다.
     };
-  }, [scrollPoint]);
-
-  //   useEffect(() => {
-  //     getPosting().then();
-  //   });
+  }, []);
 
   return (
     <Main isLoading={isLoading} bannerPage={bannerPage} postPage={postPage} />
