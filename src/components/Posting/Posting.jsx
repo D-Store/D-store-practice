e7264ba36d93d342b3f6c1da712b6a993bc77b0b
조민수 <chominsu10@gmail.com>
2021-04-21 20:11:55 +0900

    [ADD] ckEditor 5

--- src/components/Posting/Posting.jsx
+++ src/components/Posting/Posting.jsx
@@ -6,7 +6,22 @@ const Posting = () => {
     <div className="posting">
       <div className="content">
         <div className="posting-title">프로젝트 생성</div>
-        <div className="posting-form"></div>
+        <div className="posting-form">
+          <div className="posting-form-title">
+            <input
+              type="text"
+              className="posting-form-title-item"
+              placeholder="제목"
+            />
+            <div className="posting-form-content">
+              <textarea
+                className="posting-form-content-item"
+                placeholder="내용"
+              />
+            </div>
+            {/* 제목, 내용, 사진 */}
+          </div>
+        </div>
       </div>
     </div>
   );
