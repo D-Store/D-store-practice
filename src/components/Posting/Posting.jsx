60535b33742bc20a45544029516eefeb7076d074
조민수 <chominsu10@gmail.com>
2021-04-24 00:41:00 +0900

    [ADD] Posting func

--- src/components/Posting/Posting.jsx
+++ src/components/Posting/Posting.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
 import "./Posting.css";
 
-const Posting = () => {
+const Posting = ({
+  title,
+  content,
+  img,
+  setContent,
+  onChangeTitle,
+  onChangeContent,
+  onChangeImg,
+  handlePosting,
+}) => {
   return (
     <div className="posting">
       <div className="content">
@@ -12,15 +24,49 @@ const Posting = () => {
               type="text"
               className="posting-form-title-item"
               placeholder="제목"
+              value={title}
+              onChange={onChangeTitle}
             />
-            <div className="posting-form-content">
-              <textarea
-                className="posting-form-content-item"
-                placeholder="내용"
+          </div>
+          <div className="posting-form-content">
+            <CKEditor
+              className="posting-form-content-item"
+              editor={ClassicEditor}
+              data="<p></p>"
+              onReady={editor => {
+                // You can store the "editor" and use when it is needed.
+                console.log("Editor is ready to use!", editor);
+              }}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                console.log({ event, editor, data });
+                onChangeContent({ data });
+              }}
+              onBlur={(event, editor) => {
+                console.log("Blur.", editor);
+              }}
+              onFocus={(event, editor) => {
+                console.log("Focus.", editor);
+              }}
+            />
+            <div className="posting-form-content-img">
+              <input
+                type="file"
+                className="posting-form-content-img-item"
+                accept="image/jpg,image/png"
+                onChange={onChangeImg}
               />
             </div>
-            {/* 제목, 내용, 사진 */}
           </div>
+          <div className="posting-form-submit">
+            <button
+              className="posting-form-submit-item"
+              onClick={handlePosting}
+            >
+              제출
+            </button>
+          </div>
+          {/* 제목, 내용, 사진 */}
         </div>
       </div>
     </div>
--- src/containers/Posting/PostingContainer.jsx
+++ src/containers/Posting/PostingContainer.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Posting from "../../components/Posting";
 
 const PostingContainer = () => {
-  return <Posting />;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState();
+  const [img, setImg] = useState("");
+
+  const onChangeTitle = e => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeContent = ({ data }) => {
+    setContent(data);
+  };
+
+  const onChangeImg = e => {
+    setImg(e.target.files[0]);
+  };
+
+  const handlePosting = () => {
+    const formData = new FormData();
+    formData.append("file", img);
+    console.log(title, formData, content, formData.get("file"));
+  };
+
+  return (
+    <Posting
+      title={title}
+      content={content}
+      img={img}
+      setContent={setContent}
+      onChangeTitle={onChangeTitle}
+      onChangeContent={onChangeContent}
+      onChangeImg={onChangeImg}
+      handlePosting={handlePosting}
+    />
+  );
 };
 
 export default PostingContainer;
